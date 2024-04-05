1190942068309355b99a172ec8ddb030cec79175
zhanghonggang <sstraybird@163.com>
2024-04-05 16:04:58 +0800

    添加spring-context模块

--- app/src/main/java/spring/app/App.java
+++ app/src/main/java/spring/app/App.java
@@ -3,19 +3,11 @@
  */
 package spring.app;
 
-import spring.list.LinkedList;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static spring.utilities.StringUtils.join;
-import static spring.utilities.StringUtils.split;
-import static spring.app.MessageUtils.getMessage;
-
-import org.apache.commons.text.WordUtils;
 
 public class App {
     public static void main(String[] args) {
-        LinkedList tokens;
-        tokens = split(getMessage());
-        String result = join(tokens);
-        System.out.println(WordUtils.capitalize(result));
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
     }
 }
